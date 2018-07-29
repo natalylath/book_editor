5b3f34e8a9c8fa16ab0876f7fb69b678532bc984
nataly <nataly.lath@gmail.com>
2018-07-29 14:55:58 +0100

    change from raw XHR to jQuery ajax function

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,107 +1,82 @@
-String.prototype.trunc = String.prototype.trunc ||
-    function(n){
-        return (this.length > n) ? this.substr(0, n-1) + '&hellip;' : this;
-};
-
 // CHECK URL and PORT!
-const cur_url = 'http://127.0.0.1:3000';
-
-function httpGetAsync(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            callback(JSON.parse(xhr.response))
-        };
-    };
-
-    xhr.open('GET', url, true);
-    xhr.send();
+var appConfig = {
+  cur_url: 'http://127.0.0.1:3000'
 };
 
-function httpDeleteAsync(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            callback()
-        };
-    };
+var bookHelper = {
+  truncContent: function(content, n=100) {
+      if (content.length < n) {
+        return content;
+      } else {
+        return content.split('').slice(0, n-3).concat('...').join('');
+      }
+  },
 
-    xhr.open('DELETE', url, true);
-    xhr.send();
-};
+  make_new_book: function() {
+      var book = {author:$('#new_book_author').val(), title:$('#new_book_title').val(), content:$('#new_book_content').val()};
+      return book;
+  },
 
-function make_new_book() {
-    var book = {author:$('#new_book_author').val(), title:$('#new_book_title').val(), content:$('#new_book_content').val()};
-    return book;
-}
-
-function httpPostAsync(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url, true);
-
-    xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-          callback(xhr.responseText);
-        };
-    };
-    var book = make_new_book();
-    xhr.send(JSON.stringify(book));
-};
-
-function update_book() {
-    var book_id = 50
-    var book = {id: book_id, author:'Malina22', title:'Tarabam', content:'No no'};
-    return book;
-}
-
-function httpPutAsync(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('PUT', url, true);
-
-    xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-          callback(xhr.responseText);
-        };
-    };
-    var book = update_book();
-    xhr.send(JSON.stringify(book));
+  update_book: function() {
+      var book_id = 0;
+      var book = {id: book_id, author:'Malina29', title:'Tarabam', content:'No no'};
+      return book;
+  }
 };
 
 function my_requests() {
-    httpGetAsync(cur_url, function(data) {
-        mycontent ='';
+    $.ajax({
+      type: "GET",
+      url: appConfig.cur_url,
+      success: function(data) {
+        var mycontent ='';
+        data = JSON.parse(data);
         for (i in data) {
-            elem = JSON.parse(data[i])
-            mycontent = mycontent + '<tr data-id=' + i + '><td>' + elem.author + '</td>' + '<td>' + elem.title + '</td>' + '<td>' + elem.content.trunc(100) + '</td><td><button class="book-del">Delete</button></td></tr>';
+            var elem = JSON.parse(data[i]);
+            var truncedContent = bookHelper.truncContent(elem.content, 100);
+            mycontent = mycontent + '<tr data-id=' + i + '><td>' + elem.author + '</td>' + '<td>' + elem.title + '</td>' + '<td>' + truncedContent + '</td><td><button class="book-del">Delete</button></td></tr>';
         };
         $("#books_table").html(mycontent);
+      }
     });
 
 
     $('#add_book').click(function(e) {
-        httpPostAsync(cur_url, function() {
+        var newBook = bookHelper.make_new_book();
+        e.preventDefault();
+        $.ajax({
+          type: "POST",
+          url: appConfig.cur_url,
+          data: newBook,
+          success: function() {
+            console.log('Book is added');
+          }
         });
     });
 
     $('#update_book').click(function() {
-        httpPutAsync(cur_url, function() {
-        });
+      var updatedBook = bookHelper.update_book();
+      $.ajax({
+        type: "PUT",
+        url: appConfig.cur_url,
+        data: updatedBook
+      });
     });
 
     $('#books_table').on('click', '.book-del', function(e) {
         var tr = $(this).closest('tr');
-        var url = cur_url + "/book/" + tr.data('id')
 
-        httpDeleteAsync(url, function(data) {
-            tr.remove()
+        $.ajax({
+          type: "DELETE",
+          url: appConfig.cur_url + "/book/" + tr.data('id'),
+          success: function() {
+            tr.remove();
+          }
         });
+
     });
 
-}
+};
 
 $(document).ready(function() {
     my_requests();
